61e96fb07239d4604514aae27e6df8b9886d66f5
33219423 <nicekuy7868@gmail.com>
2026-03-24 22:13:10 +0700

    Add_CRUD

--- song_api/views.py
+++ song_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Song
+from .serializers import SongSerializer
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
